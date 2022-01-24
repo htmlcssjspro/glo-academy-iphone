55247f48b30dfe50900098eb9ca505a3a360697c
Sergei MILITER <militer@htmlcssjs.pro>
2022-01-25 00:52:52 +1000

    ADVANCED TASK 4

--- js/main.js
+++ js/main.js
@@ -74,19 +74,88 @@ const changeActiveTabs = $button => {
 };
 
 
-const getData = () => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error('Ошибка получения данных');
-            }
-        })
-        .then(data => {console.log(data);})
-        .catch(error => console.error(error))
-        .finally(console.log('finally'));
+const list = document.querySelector('.cross-sell__list');
+
+const render = data => {
+    data.forEach(item => {
+        list.insertAdjacentHTML('beforeend', `
+            <li>
+                <article class="cross-sell__item">
+                    <img class="cross-sell__image" src="${item.photo}" alt="${item.id}">
+                    <h3 class="cross-sell__title">${item.name}</h3>
+                    <p class="cross-sell__price">${item.price}₽</p>
+                    <button type="button" class="button button_buy cross-sell__button">Купить</button>
+                </article>
+            </li>
+        `);
+    });
 };
+
+
+
+
+const getData = () => {
+    return fetch('./cross-sell-dbase/dbase.json')
+        .then(response => response.json())
+        .catch(error => console.error(error));
+};
+
+
+const stack = 4;
+let begin = 0;
+
+// const sliceArray = (data) => data.slice(position, stack);
+
+const changeData = () => {
+    getData().then(data => {
+        const end = begin + stack;
+        render(data.slice(begin, end));
+
+        if (data.length > end) {
+            begin += stack;
+        } else {
+            document.querySelector('.cross-sell__add').style.display = 'none';
+        }
+    });
+};
+
+
+const $modal = document.querySelector('.modal');
+const $form = $modal.querySelector('form');
+
+const modalOpen = () => {
+    const title = document.querySelector('.card-details__title').textContent;
+    $modal.querySelector('.modal__title').textContent = title;
+    $modal.style.display = 'flex';
+};
+const modalClose = () => {
+    $modal.style.display = '';
+};
+
+const sendForm = () => {
+    const formData = {};
+    const labels = $form.querySelectorAll('.modal__label');
+    labels.forEach(label => {
+        const span = label.querySelector('span');
+        const input = label.querySelector('input');
+        formData[span.textContent] = input.value;
+    });
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method:  'POST',
+        body:    JSON.stringify(formData),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json);
+            $form.reset();
+            modalClose();
+        });
+};
+
 
 document.body.addEventListener('click', event => {
     event.preventDefault();
@@ -104,41 +173,62 @@ document.body.addEventListener('click', event => {
     const $button = t.closest('button.card-detail__change');
     $button && changeActiveTabs($button);
 
-    // characteristics__list
-    //     characteristics__item
-    //         characteristics__title
-    //         characteristics__description active
-
-    /**
-     * @type {HTMLElement}
-     */
-    const $chParent = t.closest('.characteristics__list');
     /**
      * @type {HTMLElement}
      */
     const $chItem = t.closest('.characteristics__item');
+    /**
+     * @type {HTMLElement}
+     */
+    const $chParent = t.closest('.characteristics__list');
 
     if ($chParent && $chItem) {
-        const $btn = $chItem.querySelector('.characteristics__title');
+        const $title = $chItem.querySelector('.characteristics__title');
         const $description = $chItem.querySelector('.characteristics__description');
 
         const $active = $chParent.querySelector('.characteristics__title.active');
         const $open = $chParent.querySelector('.characteristics__description.open');
 
-        $active && $active !== $btn && $active.classList.remove('active');
+        $active && $active !== $title && $active.classList.remove('active');
 
         if ($open && $open !== $description) {
             $open.classList.remove('open');
             $open.style.height = '0px';
         }
 
-        $btn.classList.toggle('active');
+        $title.classList.toggle('active');
         $description.classList.toggle('open');
         $description.style.height = $description.classList.contains('open')
             ? $description.scrollHeight + 'px' : '0px';
     }
+
+    /**
+     * @type {HTMLElement}
+     */
+    const $btnAdd = t.closest('.cross-sell__add');
+    $btnAdd && changeData();
+
+
+
+    /**
+     * @type {HTMLElement}
+     */
+    const $btnOpenModal = t.closest('.card-details__button_delivery');
+    $btnOpenModal && modalOpen();
+
+    /**
+     * @type {HTMLElement}
+     */
+    const $modalClose = t.closest('.modal__close');
+    $modalClose && modalClose();
+
+    /**
+     * @type {HTMLElement}
+     */
+    const $submit = t.closest('[type=submit]');
+    $submit && sendForm();
 });
 
 
 changeActiveTabs();
-getData();
+changeData();
